ba2463e8e177d52d5abce21e7afa8e0002ec45b3
Juan Bermudez <juan.bermudez_m@uao.edu.co>
2026-02-25 18:36:25 -0500

    Implementacion sumatoria de cada cuota como "Total prestado"

--- Funcionalidad/main.js
+++ Funcionalidad/main.js
@@ -1,21 +1,50 @@
-import { valorCuota } from "./operaciones.js";
+import { listarDatos, valorCuota, totalPrestado /*sumarCuotas*/} from "./operaciones.js";
 const botonCalcular = document.getElementById("calcular");
 const botonHistorial = document.getElementById("recordar");
 const laSalida = document.getElementById("resultadoPrestamo");
+const botonTotalPrestado = document.getElementById("totalPrestado");
 
 botonCalcular.addEventListener("click", calcularCuota);
 botonHistorial.addEventListener("click", recordarHistorial);
+botonTotalPrestado.addEventListener("click", mostrarTotalPrestado);
+
+let historial = [];
 
 function calcularCuota() {
     let nombre = document.getElementById("elNombre").value
     let prestamo = parseFloat(document.getElementById("elPrestamo").value)
     let meses = parseInt(document.getElementById("losMeses").value)
     let intereses = parseFloat(document.getElementById("losInteres").value)
+    
+    let cuota = valorCuota(prestamo, intereses, meses).toFixed(0)
 
-    console.log(nombre, prestamo, meses, intereses)
-    laSalida.textContent = "La cuota es: " + valorCuota(prestamo, intereses, meses)
+    let registro = {
+        nombre,
+        prestamo,
+        meses,
+        intereses,
+        cuota
+        };
+
+    historial.push(registro);
+
+    laSalida.textContent = nombre + " debe pagar $" 
+    + cuota + " cada mes por el prestamo de $"
+    + prestamo + " a " + meses + " meses con el interes del " + intereses + "%";
+    return historial;
 }
 
 function recordarHistorial() {
-    laSalida.textContent = "Estas en el historial"
+    console.log(historial)
+    laSalida.textContent = listarDatos(historial)
 }
+
+function mostrarTotalPrestado() {
+    console.log(totalPrestado(historial))
+    laSalida.textContent = totalPrestado(historial)
+}
+
+/*function mostrarSumatoriaCuotas() {
+    const resultado = sumarCuotas(historial);
+    laSalida.textContent = resultado;
+}*/
--- Funcionalidad/operaciones.js
+++ Funcionalidad/operaciones.js
@@ -1,7 +1,41 @@
 function valorCuota(prestamo, i, n) {
     i = i/100
     let aux = Math.pow((1+i),n)
-    let oper = prestamo * ((aux*1)/(aux-1))
+    let oper = prestamo * ((aux*i)/(aux-1))
     return oper;
 }
+
+function listarDatos(info){
+    let save = ''
+    info.forEach(persona => {
+        save += 'Nombre: ' + persona.nombre 
+        + ' pidió ' + persona.prestamo 
+        + ' a ' + persona.meses
+        + ' meses con intereses de ' + persona.intereses
+        + '% y cuota ' + persona.cuota + '\n';
+    });
+    console.log(save)
+    return save;
+};
+
+function totalPrestado(h) {
+    let prestado = 0
+    h.forEach(element => {
+        prestado += parseFloat(element.cuota);
+    });
+    return prestado.toFixed(2);
+}
+
+/*function sumarCuotas(historial) {
+    let total = 0;
+    historial.forEach(registro => {
+        // Convertimos a número antes de sumar
+        total += parseFloat(registro.cuota);
+    });
+    return "La sumatoria de todas las cuotas es: $" + total.toFixed(2);
+}*/
+
+//export {sumarCuotas}
+export {totalPrestado}
+export {listarDatos};
 export {valorCuota};
